b1c5b86f674175da2180825eec76021739abf4f7
Simon Fell <fellforce@gmail.com>
2010-12-04 10:44:47 -0800

    Updated version# in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name = "beatbox",
-      version = "0.9",
+      version = "0.91",
       py_modules = ["beatbox", "xmltramp"],
       description = "Makes the salesforce.com SOAP API easily accessible.",
       author = "Simon Fell",
